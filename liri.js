f8b1895e9422c7f3fc349cc67f6696401a976e42
Eleni Pastra <hell-en@Elenis-MacBook-Pro.local>
2018-08-07 19:46:58 -0700

    functional omdb + tweetycode

--- liri.js
+++ liri.js
@@ -3,59 +3,56 @@ var stuffINeed = require("./keys.js")
 var request = require("request");
 var fs = require("fs");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var operator = process.argv[2];
+var searchTerm = process.argv.slice(3).join(" ");
+console.log("hi");
 
 
-// * `my-tweets`
-var search = process.argv[2];
-console.log(search);
-var tweetSearch = process.argv.slice(3).join(" ");
-console.log(tweetSearch);
-// // var MyTweets = function () {
-// //     var divider = "\n-------------------------------";
+if(operator === "movie-this"){
+    movie();
+}
+else if(operator === "my-tweets"){
+  myTweets();
+
+function myTweets() {
+    var divider = "\n-------------------------------";
 // //     this.loadTweets = function (tweets) {
-// //         request(client, function (error, response, body) {
-// //             if (error) {
-// //                 console.log(error);
-// //                 return
-// //             };
-// //             var jsonData = JSON.parse(body);
-// //             var showData = [
-// //                 "Show: " + jsonData.name,
-// //                 "Genre: " + jsonData.genres.join(", "),
-// //                 "Rating: " + jsonData.rating.average,
-// //                 "Network: " + jsonData.network.name,
-// //                 "Summary: " + jsonData.summary
-// //             ].join("\n\n");
+    // var URL = "https://api.twitter.com/1.1/search/tweets.json?q="+ tweetSearch + "%20new%20premium
+        request(client, function (error, response, body) {
+            if (error) {
+                console.log(error);
+                return
+            };
+        
+            var jsonData = JSON.parse(body);
+            var tweetData = [
+                "user: " + jsonData.user.name,
+                "Tweet: " + jsonData.text,
+                "Posted On: " + jsonData.created_at,
+            ].join("\n\n");
 
-// //             fs.appendFile("log.txt", showData + divider, function (error) {
-// //                 if (error) throw error;
-// //                 console.log(showData);
-// //             });
-// //         });
-// //     };
+            fs.appendFile("log.txt", tweetData + divider, function (error) {
+                if (error) throw error;
+                console.log(tweetData);
+            });
+        })
+    };
 // }
 
 // * `spotify-this-song`
 
-// node liri.js spotify-this-song '<song name here>'
-var search = process.argv[2];
-console.log(search);
-var songSearch = process.argv.slice(3).join(" ");
-console.log(songSearch);
+// // node liri.js spotify-this-song '<song name here>'
+// var songSearch = process.argv.slice(3).join(" ");
+// console.log(songSearch);
 
 
 // * `movie-this`
-var search = process.argv[2];
-console.log(search);
-var movieSearch = process.argv.slice(3).join(" ");
-console.log(movieSearch)
-var Movie = function () {
+function movie() {
     var divider = "\n-------------------------------";
-    this.findMovie = function (show) {
 
-        var URL = "http://www.omdbapi.com/?i=tt3896198&apikey=d41a7cfc" + show;
+        var URL = "http://www.omdbapi.com/?t=" + searchTerm + "&apikey=d41a7cfc";
 
         request(URL, function (error, response, body) {
             if (error) {
@@ -64,15 +61,17 @@ var Movie = function () {
             };
             var jsonData = JSON.parse(body);
 
+            console.log(jsonData, null, 2);
+
             var movieData = [
-                "Title: " + jsonData.title,
-                "Year: " + jsonData.year,
-                "IMDB Rating: " + jsonData.ratings.source[0].value,
-                "Rotten Tomatoes Rating: " + jsonData.ratings.source[1].value,
-                "Country: " + jsonData.country,
-                "Language: " + jsonData.language,
-                "Plot: " + jsonData.plot,
-                "Actors: " + jsonData.actors.join(", ")
+                "Title: " + jsonData.Title,
+                "Year: " + jsonData.Year,
+                "IMDB Rating: " + jsonData.Ratings[0].Source.Value,
+                "Rotten Tomatoes Rating: " + 'no rating' || jsonData.Ratings[1].Source.Value,
+                "Country: " + jsonData.Country,
+                "Language: " + jsonData.Language,
+                "Plot: " + jsonData.Plot,
+                "Actors: " + jsonData.Actors
             ].join("\n\n");
 
             fs.appendFile("log.txt", movieData + divider, function (error) {
@@ -80,8 +79,6 @@ var Movie = function () {
                 console.log(movieData);
             });
         });
-    }
 };
-var search = process.argv[2];
-
+}
 // * `do-what-it-says`
